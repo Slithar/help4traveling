42ace6ed761fbe7e24dcd79e7b7c79bc667e510f
Roccma <mauroroccaf@gmail.com>
2016-08-04 02:38:46 -0300

    Hice la prueba con el frame

--- Nuevo/src/Presentacion/Presentacion.java
+++ Nuevo/src/Presentacion/Presentacion.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package Presentacion;
+import Presentacion.help4travelingUI;
+import Presentacion.Presentacion;
+import javax.swing.*;
 import java.util.*;
 /**
  *
@@ -11,8 +14,13 @@ import java.util.*;
  */
 public class Presentacion {
     public static void main (String [ ] args) {
+        
+        System.out.println("Acá empece");
  
-          help4travelingUI nuevo = new help4travelingUI();
-          nuevo.setVisible(true);
-        }
+       /* help4travelingUI nuevo = new help4travelingUI();
+        nuevo.setVisible(true);
+        nuevo.setBounds(100, 100, 500, 250);*/
+       pruebaFrame nuevo = new pruebaFrame();
+       nuevo.setVisible(true);
+    }
 }
